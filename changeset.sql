fb81378da23f13dfab77ce66d0f5dc26438d84f9
dado_hp <odadph@gmail.com>
2020-05-14 11:20:45 +0700

    add table test2

--- changeset.sql
+++ changeset.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
---changeset author:1 
-create table test3 ( 
+--changeset author:2 
+create table test2 ( 
 id int primary key,
 name varchar(255)
 );
---rollback drop table test3;
+--rollback drop table test2;
